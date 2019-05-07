2d3339fc8156beaa4260040ae0abbb8b386617e5
CoderHam <hemanham@gmail.com>
2019-05-07 16:41:24 -0700

    placeholder files for torch backend

--- libtorch_test/libtorch_backend.cc
+++ libtorch_test/libtorch_backend.cc
@@ -14,8 +14,16 @@ int main(int argc, const char* argv[]) {
   std::shared_ptr<torch::jit::script::Module> module = torch::jit::load(argv[1]);
 
   assert(module != nullptr);
-  std::cout << "ok\n";
+  std::cout << "Model loaded successfully\n";
 
-  torch::Tensor tensor = torch::rand({2, 3});
-  std::cout << tensor << std::endl;
+  torch::Tensor input_tensor = torch::rand({1, 3, 224, 224});
+  // std::cout << input_tensor << std::endl;
+  // Create a vector of inputs.
+  std::vector<torch::jit::IValue> inputs;
+  inputs.push_back(input_tensor);
+
+  // Execute the model and turn its output into a tensor.
+  at::Tensor output = module->forward(inputs).toTensor();
+
+  std::cout << output.slice(/*dim=*/1, /*start=*/0, /*end=*/5) << '\n';
 }
